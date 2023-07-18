6d2484416dd6aa1eed50b031619bc045028d6933
Nazan Bas <n-8888@t-online.de>
2023-07-19 00:03:17 +0200

    new kata

---
+++ Quote-API/API.js
@@ -0,0 +1,22 @@
+"use strict";
+// Aufgabe : über den button soll zufalls zitate angezeigt werden
+//über die fetch API die daten ziehen und über den button einen zufälligen  im main anzeigen lassen
+//adresse über die wir fetchen müssen 'https://dummy-apis.netlify.app/api/quote'
+
+//variablen anlegen für den button & main
+const buttonGet = document.querySelector(".button");
+const quoteField = document.querySelector(".quoteField");
+const authorField = document.querySelector(".authorField");
+
+function randomQuote() {
+  fetch("https://dummy-apis.netlify.app/api/quote")
+    .then((response) => response.json())
+    .then((quoteData) => {
+      quoteField.innerHTML = quoteData.quote;
+      authorField.innerHTML = quoteData.author;
+    });
+}
+
+randomQuote();
+
+buttonGet.addEventListener("click", randomQuote);
